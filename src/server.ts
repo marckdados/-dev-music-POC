094870d4a581d57dc7a36287fdabba05b2a7a87e
Bismarck <bismarckdados@gmail.com>
2023-01-20 20:51:20 -0300

    feat: add express and test router health

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,7 @@
-console.log("Hello");
+import express, { json } from "express";
+import router from "./routers/index.js";
+const app = express();
+app.use(json());
+app.use(router);
+
+app.listen(4000, () => console.log("Running in port 4000"));
